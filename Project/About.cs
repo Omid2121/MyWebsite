58e95233fdc0f75dfbd1c5e18cc598823a05d551
Omid2121 <59820333+Omid2121@users.noreply.github.com>
2020-12-01 08:09:32 +0100

    01.12.20

---
+++ Project/About.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AboutNameSpace
+{
+    public class About
+    {
+        public string Image { get; set; }
+        public string HeadingName { get; set; }
+        public string Text { get; set; }
+    }
+    public static class Abouts
+    {
+        public static List<About> list = new List<About>
+        {
+            new About
+            {
+                HeadingName = "Omidreza Ahangi",
+            },
+            new About
+            {
+                HeadingName = "Uddannelse",
+                Text = "Om min uddannelse.",
+            },
+            new About
+            {
+                HeadingName = "Om Mig",
+                Text = "Om mig.",
+            },
+            new About
+            {
+                HeadingName = "Min fritid",
+                Text = "Min fritider.",
+                Image = "image/me.jpg",
+
+            }
+        };
+    }
+}
---
+++ Project/Projects.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ProjectsNamespace
+{
+    public class Project
+    {
+        public string Image { get; set; }
+        public string Name { get; set; }
+        public string Text { get; set; }
+        public string DownloadBtn { get; set; }
+    }
+
+    public static class Projects
+    {
+        public static List<Project> list = new List<Project>
+        {
+            new Project
+            {
+                Image = "497_449074_management.hero.jpg",
+                Name = "Lagersystem",
+                Text = "Jeg lavet dette projekt til GUI/OOP H2 aflevering. Med dette program man kan tilføje enhed, redigere og slet det.",
+                DownloadBtn = "",
+            },
+            new Project
+            {
+                Image = "497_449074_management.hero.jpg",
+                Name = "Lommeregner",
+                Text = "Jeg lavet dette projekt til H2 clientside aflevering, Det er en lommeregner som virker med javascript og jQuery.",
+                DownloadBtn = "",
+            },
+            new Project
+            {
+                Image = "497_449074_management.hero.jpg",
+                Name = "Pizzeria Menu",
+                Text = "Jeg lavet dette project i skolepraktik, Det er en menu til pizzeria, man kan tilføje en item, redigere og slet.",
+                DownloadBtn = "",
+            }
+        };
+    }
+}
